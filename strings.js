a4282dd7e9a59d7bfc53102b52c5078070df986f
dhazlettjr <d.hazlett.jr@gmail.com>
2017-12-21 16:05:20 -0600

    created function to put output in alphabetical order

--- strings.js
+++ strings.js
@@ -8,31 +8,40 @@ let output = document.getElementById("output");
 let input = document.getElementById("input");
 
 
-
+//use the ID and attach an EL to it
 document.getElementById("btn").addEventListener("click", function(){
+
+// call this function inside the EL function in order for it to fire
     reversal(input.value);
-   // document.getElementById("output").innerHTML = input.value;
+    alphabits(input.value);
+
 });
 
-
+// create a function to reverse the string
 function reversal(str) {
 
-let splitString = str.split("");
-let reverseArr = splitString.reverse();
-let joinArr = reverseArr.join("");
+// create variables to name split, reverse and join
+    let splitString = str.split("");
+    let reverseArr = splitString.reverse();
+    let joinArr = reverseArr.join("");
+
     document.getElementById("output").innerHTML = str.split("").reverse().join("");
-    //return str.split("").reverse().join("");
+
+}
+
+//Implement the logic in the alphabits function that return the characters in alphabetical order, and outputs the result in the DOM, below the text input.
+
+function alphabits(str) {
+    let newArr = str.split("");
+    let alphaB = newArr.sort();
+    let merge = alphaB.join("");
+
+    document.getElementById("output").innerHTML = str.split("").sort().join("");
 
 }
 
 
 
-
-
-
-function alphabits() {
-
-}
 
 function palindrome() {
 
